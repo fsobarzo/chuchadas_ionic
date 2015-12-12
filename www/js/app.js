782db38af4798e283ef409aacc4aa77945846202
Patricio Ramirez <anek@localhost.localdomain>
2015-12-12 17:10:51 -0300

    base ionic, querys, histories

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,47 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform, $http) {
+var mainIonic  = angular.module('starter', ['ionic']);
+
+mainIonic.config(function($stateProvider,$urlRouterProvider) {
+  $stateProvider
+    .state('tabs',{
+      url: '/tab',
+      abstract: true,
+      templateUrl: 'templates/base.html'
+    })
+    .state('tabs.home',{
+      url: '/home',
+      views:{
+        'home-tab':{
+          templateUrl: '/templates/home.html',
+          controller: 'HomeController'
+        }
+      }
+    })
+    .state('tabs.history',{
+      url: '/history',
+      views: {
+        'history-tab': {
+          templateUrl: '/templates/history.html',
+          controller: 'HistoriesController'
+        }
+      }
+    })
+    .state('tabs.charges',{
+      url: '/charges',
+      views: {
+        'charges-tab':{
+          templateUrl: '/templates/charges.html',
+          controller: 'ChargesController'
+        }
+      }
+    });
+  $urlRouterProvider.otherwise('/tab/home');
+  });
+
+mainIonic.run(function($ionicPlatform, $http) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -15,35 +53,57 @@ angular.module('starter', ['ionic'])
       // Don't remove this line unless you know what you are doing. It stops the viewport
       // from snapping when text inputs are focused. Ionic handles this internally for
       // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
+      // cordova.plugins.Keyboard.disableScroll(true);
     }
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
-})
+});
 
-.controller('AppCtrl', function($scope, $http){
+mainIonic.controller('HomeController', function($scope, $http){
   $scope.platform = ionic.Platform.platform();
 
-  $http.get('http://localhost:5000'+'/users').
+  $http.get('http://204.87.169.50:5000'+'/users').
     success(function(data,status,headers,config){
-      $scope.players = data.users
+      $scope.users = data.users
     });
 
   $scope.create = function(player){
-    $http.post('http://localhost:5000'+'/users', player)
+    $http.post('http://204.87.169.50:5000'+'/users', player)
       .success(function(data, status, headers, config) {
-        user = data.user;
-        console.log(user);
+        $scope.users.push(data.user);
       })
   }
 
   $scope.addChuchada = function(player){
-    $http.put('http://localhost:5000'+'/users/' + player.username)
+    $http.put('http://204.87.169.50:5000'+'/users/' + player.username + '/chuchada')
       .success(function(data, status, headers, config) {
-        user = data.user;
-        
+        angular.forEach($scope.users,function(value, key){
+          if(value.id == data.user.id)
+            $scope.users[key] = data.user;
+        });
       });
   }
-})
+});
+
+
+mainIonic.controller('ChargesController', function($scope, $http){
+  $scope.platform = ionic.Platform.platform();
+});
+
+mainIonic.controller('HistoriesController', function($scope, $http){
+  $scope.platform = ionic.Platform.platform();
+  $http.get('http://204.87.169.50:5000'+'/histories/').
+  success(function(data,status,headers,config){
+    $scope.histories = data.histories;
+  });
+
+  $scope.refresh = function(){
+    $scope.histories = [];
+    $http.get('http://204.87.169.50:5000'+'/histories/').
+      success(function(data,status,headers,config){
+        $scope.histories = data.histories;
+      });
+  }
+});
